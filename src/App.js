3e6d6779b5de74de743b5d99e3c0aec0ab84bf1f
Gersande <write@gersande.com>
2016-04-03 17:10:00 -0400

    errr... added images?

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ const App = () => (
   <div className={styles.butts}>
     <div className={styles.introduction}>
       <p className={styles.headline}>lets do marnifersto???</p>
-      <p className={styles.byline}>by lana gersande and six</p>
+      <p className={styles.byline}>a manifesto against social media engagement</p>
+      <a className={styles.twittershare}>shr on twerttr</a>
+      <ul className={styles.tableofcontents}>
+        <li>block</li>
+        <li>relish</li>
+        <li>perf</li>
+        <li>sells</li>
+        <li>void</li>
+      </ul>
     </div>
   </div>
 );
