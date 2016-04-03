f5649ef22cbc786bacada8953eaeb7163f386d8c
Gersande <write@gersande.com>
2016-04-03 14:31:09 -0400

    made edits to css and html

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import styles from './App.module.css';
 
 const App = () => (
   <div className={styles.butts}>
-    <p>Let's do marnifersto???</p>
-    <div>
+    <div className={styles.marniferstointroduction}>
+      <p className={styles.marniferstoheadline}>lets do marnifersto???</p>
+      <p className={styles.marniferstobyline}>by lana gersande and six</p>
     </div>
   </div>
 );
