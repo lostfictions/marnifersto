b94e186f99d856d62823ca29ba102f7f64f47e1c
Gersande <write@gersande.com>
2016-04-03 13:22:36 -0400

    editing css

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ const App = () => (
   <div className={styles.butts}>
     <p>Let's do marnifersto???</p>
     <div>
-      <h1>block boring people</h1>
-      <h1>there is no authentic self</h1>
-      <h1>you are performance art</h1>
-      <h1>performance is labour</h1>
-      <h1>relish awkward silences</h1>
-      <h1>for there to be a universe, there needs to be a void</h1>
     </div>
   </div>
 );
